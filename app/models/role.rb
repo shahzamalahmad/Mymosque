943b6af147a6a04ebea8ba99b74a1fe63ddde8b4
Shahzamal Ahmad <shahzamalahmad@Shahzamals-MacBook-Air.local>
2025-02-10 15:26:44 +0530

    admin and home poage links added

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ApplicationRecord
   self.table_name = :roles
+
+  belongs_to :user
 end
 
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,6 +5,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Mymosque"
+  config.site_title_link = "http://localhost:3000/homes"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
